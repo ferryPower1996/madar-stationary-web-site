6a2e69e255f81b41e872d800ba049e388d58785d
Farhan <farhan.razzaghi@hatmail.com>
2023-09-04 21:24:25 +0330

    new changes

--- src/ads_and_logo.jsx
+++ src/ads_and_logo.jsx
@@ -2,7 +2,7 @@ import './ads_and_logo.css'
 const Ads_and_logo = ({changeBackgroundImage,srcimage}) => {
     return ( <>
     <div className="container">
-    <h2>برای حالت دارک مود و لایت مود روی لوگو کلیک کنید</h2>
+    {/* <h2>برای حالت دارک مود و لایت مود روی لوگو کلیک کنید</h2> */}
     <div className="ads_and_logo">
         <div className="ads"></div>
         <div className="ads"></div>
